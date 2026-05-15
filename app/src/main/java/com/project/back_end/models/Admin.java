40d5f8b4f9f51e48776f57d393f34b596a43cc60
vm0303 <vmadhav33@gmail.com>
2026-05-15 00:03:24 -0400

    Added missing imports and updated application.properties
    updated the application.properties to take in export values

--- app/src/main/java/com/project/back_end/models/Admin.java
+++ app/src/main/java/com/project/back_end/models/Admin.java
@@ -1,5 +1,8 @@
 package com.project.back_end.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 @Entity
 @Table(name ="admin")
 public class Admin {
